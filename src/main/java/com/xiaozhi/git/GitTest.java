a1e4b304728e0b6878cffb87546cba27c4c7d9f3
xiaozhi <xiaozhi@qq.com>
2021-05-04 01:36:32 +0800

    second commit

---
+++ src/main/java/com/xiaozhi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xiaozhi.git;
+
+/**
+ * @author xiaozhi
+ * @description
+ * @create 2021-05-2021/5/4 1:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("second commit");
+    }
+}
