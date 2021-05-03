4a69f2d389177883542347c315e968d33e6b8c87
xiaozhi <xiaozhi@qq.com>
2021-05-04 01:45:43 +0800

    second commit

--- src/main/java/com/xiaozhi/git/GitTest.java
+++ src/main/java/com/xiaozhi/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("second commit");
         System.out.println("third commit");
+        System.out.println("hot-fix commit");
     }
 }
